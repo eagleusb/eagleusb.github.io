ab65107d371cc4b0a3eda4d34873877870e3cc78
eagleusb <git@eagleusb.com>
2020-08-30 12:48:35 +0200

    feat(url) dynamic url bar ftw

--- static/assets/cat.js
+++ static/assets/cat.js
@@ -2,4 +2,18 @@ feather.replace({
   class: 'feather',
   'stroke-width': 1,
   color: '#1d6cd7',
-})
+});
+
+function loop() {
+  var i, n, s = '';
+
+  for (i = 0; i < 10; i++) {
+    n = Math.floor(Math.sin(Date.now() / 200 + i / 2) * 4) + 4;
+    s += String.fromCharCode(0x2581 + n);
+  }
+
+  window.location.hash = s;
+  setTimeout(loop, 50);
+}
+
+loop();
